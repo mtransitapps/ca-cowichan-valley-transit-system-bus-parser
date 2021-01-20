e6602e76fcee0706979268831e0a3cb7099583be
Mathieu Méa <mathieu.mea@gmail.com>
2021-01-20 09:35:58 -0500

    GTFS Direction Finder > enable

--- src/main/java/org/mtransit/parser/ca_cowichan_valley_transit_system_bus/CowichanValleyTransitSystemBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_cowichan_valley_transit_system_bus/CowichanValleyTransitSystemBusAgencyTools.java
@@ -5,29 +5,18 @@ import org.jetbrains.annotations.Nullable;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.StringUtils;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
-import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -155,230 +144,17 @@ public class CowichanValleyTransitSystemBusAgencyTools extends DefaultAgencyTool
 		return super.getRouteColor(gRoute);
 	}
 
-	private static final String TRAIL = "Trail";
-
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-
-	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		//noinspection deprecation
-		map2.put(5L, new RouteTripSpec(5L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Duncan", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Eagle Hts") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList( //
-								"106205", // Polkey at Mearns (EB) #EagleHts
-								"106225", // ++
-								"104033" // Central at Cowichan (NB) #Duncan
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList( //
-								"104033", // Central at Cowichan (NB) #Duncan
-								"136113", // ++
-								"106205" // Polkey at Mearns (EB) #EagleHts
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(7L, new RouteTripSpec(7L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Duncan", //
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Cowichan Lk") //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList( //
-								"136191", // South Shore 10 block (WB) #CowichanLake
-								"136457", // == Cowichan Lake at Greendale (EB)
-								"136397", // != Cowichan Valley at Skutz Falls (EB)
-								"136034", // != Canada at Station (SB)
-								"136202", // != Cowichan Lake at Lake Park (EB)
-								"136110", // != Government at Station (EB)
-								"104033" // == Central at Cowichan (NB) #Duncan
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList( //
-								"104033", // Central at Cowichan (NB) #Duncan
-								"106148", // == Canada at Station (NB)
-								"106131", // != Beverly at Duncan St (EB)
-								"106159", // != Somenos at Cowichan Valley Hwy (NB)
-								"136002", // != Ingram at Jubilee (WB)
-								"136188", // != Cowichan Lake at Lake Park (WB)
-								"136189", // == Cowichan Lake at Greendale (WB)
-								"136190", // != Cowichan Lake at Stanley (WB)
-								"136191", // xx South Shore 10 block (WB) #CowichanLake
-								"136195", // != South Shore at Stone (SB)
-								"136427", // != Somenos at Cowichan (NB)
-								"136191" // xx South Shore 10 block (WB) #CowichanLake
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(8L, new RouteTripSpec(8L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Duncan", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Mill Bay") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList( //
-								"108060", // == Huckleberry at Noowick (NB) #MillBay
-								"104000", // == Lodgepole at Frayne (NB)
-								"108052", // != Deloume at Lodge Pole Rd (EB)
-								"108090", // == Deloume Rd at Barry (NB)
-								"136295", // xx Mill Bay at Handy (SB)
-								"108006", // != Shawnigan Lake-Mill Bay at Trans Canada (WB)
-								"108162", // ==
-								"108056", // !=
-								"104006", // !=
-								"136280", // ==
-								"104033" // Central at Cowichan (NB) #Duncan
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList( //
-								"104033", // Central at Cowichan (NB) #Duncan
-								"136373", // Cowichan Bay at Telegraph (SB)
-								"108051", // ==
-								"106201", // !=
-								"106202", // !=
-								"108048", // ==
-								"108038", // != Kilmalu AT Church Way (WB)
-								"136295", // xx Mill Bay at Handy (SB)
-								"136296", // == Mill Bay at Bay (SB)
-								"136300", // != Mill Bay at Ferry (NB)
-								"108060" // == Huckleberry at Noowick (NB) #MillBay
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(9L, new RouteTripSpec(9L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Duncan", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Mill Bay") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList( //
-								"108060", // Huckleberry at Noowick (NB) #MillBay
-								"108090", // !=
-								"136295", // xx Mill Bay at Handy (SB)
-								"136302", // !=
-								"108019", // ==
-								"106201", // !=
-								"106202", // !=
-								"108018", // ==
-								"106237", // !=
-								"108089", // <>
-								"136234", // !=
-								"104033" // Central at Cowichan (NB) #Duncan
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList( //
-								"104033", // Central at Cowichan (NB) #Duncan
-								"104021", // == !=
-								"104020", // !=
-								"108089", // != <>
-								"136248", // == !=
-								"108059", // !=
-								"136295", // xx Mill Bay at Handy (SB)
-								"136296", // !=
-								"108060" // Huckleberry at Noowick (NB) #MillBay
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(20L, new RouteTripSpec(20L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Cowichan Lk", //
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Youbou") //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList( //
-								"108045", // Youbou Rd 10700 Block (NB)
-								"136193", // ++
-								"108088" // 20 Block South Shore Rd (WB) #CowichanLk
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList( //
-								"108088", // 20 Block South Shore Rd (WB) #CowichanLk
-								"108172", // ++
-								"108045" // Youbou Rd 10700 Block (NB)
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(21L, new RouteTripSpec(21L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Cowichan Lk", //
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Honeymoon Bay") //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList( //
-								"108168", // South Shore AT Park Ave (WB) #HoneymoonBay
-								"136200", // ++
-								"108088" // 20 Block South Shore Rd (WB) #CowichanLk
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList( //
-								"108088", // 20 Block South Shore Rd (WB) #CowichanLk
-								"136196", // ++
-								"108168" // South Shore AT Park Ave (WB) #HoneymoonBay
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(31L, new RouteTripSpec(31L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Ladysmith", //
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Alderwood") //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList( //
-								"106078", // Birchwood at Maplewood (EB) #Alderwood
-								"106063", // ++
-								"106050" // 1st Ave at Symonds St (SB) #Ladysmith
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList( //
-								"106050", // 1st Ave at Symonds St (SB) #Ladysmith
-								"106062", // ++
-								"106078" // Birchwood at Maplewood (EB) #Alderwood
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(34L, new RouteTripSpec(34L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Ladysmith", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Chemainus") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList( //
-								"136142", // Pine at Lang (WB) #Chemainus
-								"106095", // ++
-								"106050" // 1st Ave at Symonds St (SB) #Ladysmith
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList( //
-								"106050", // 1st Ave at Symonds St (SB) #Ladysmith
-								"106098", // ++
-								"136142" // Pine at Lang (WB) #Chemainus
-						)) //
-				.compileBothTripSort());
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
-	@Override
-	public int compareEarly(long routeId, @NotNull List<MTripStop> list1, @NotNull List<MTripStop> list2, @NotNull MTripStop ts1, @NotNull MTripStop ts2, @NotNull GStop ts1GStop, @NotNull GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@NotNull
-	@Override
-	public ArrayList<MTrip> splitTrip(@NotNull MRoute mRoute, @Nullable GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@NotNull
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(@NotNull MRoute mRoute, @NotNull GTrip gTrip, @NotNull GTripStop gTripStop, @NotNull ArrayList<MTrip> splitTrips, @NotNull GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
 	@Override
 	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
 		mTrip.setHeadsignString(
 				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
 				gTrip.getDirectionIdOrDefault()
 		);
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
 	}
 
 	private static final String EXCH = "Exch";
@@ -394,6 +170,7 @@ public class CowichanValleyTransitSystemBusAgencyTools extends DefaultAgencyTool
 	private static final Pattern DASH_VIA_ = Pattern.compile("(- via )", Pattern.CASE_INSENSITIVE);
 	private static final String DASH_VIA_REPLACEMENT = " via ";
 
+	private static final String TRAIL = "Trail";
 	private static final Pattern KEEP_TRAIL = Pattern.compile(String.format("((^|\\W){1}(%s)(\\W|$){1})", "trl"), Pattern.CASE_INSENSITIVE);
 	private static final String KEEP_TRAIL_REPLACEMENT = String.format("$2%s$4", TRAIL);
 
@@ -414,16 +191,6 @@ public class CowichanValleyTransitSystemBusAgencyTools extends DefaultAgencyTool
 
 	@Override
 	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
-		if (mTrip.getRouteId() == 4L) {
-			if (Arrays.asList( //
-					"East", //
-					"Maple Bay" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Maple Bay", mTrip.getHeadsignId());
-				return true;
-			}
-		}
 		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 	}
 
